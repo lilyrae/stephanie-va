143d2c906ca9e2948c1ad43799761fec2d55064e
Lily <lily.bickerstaffe@live.co.uk>
2017-11-24 16:40:18 +0700

    Removed unneeded comments from wikipedia module

--- Stephanie/Modules/wikipedia_module.py
+++ Stephanie/Modules/wikipedia_module.py
@@ -3,17 +3,15 @@ from urllib.error import URLError
 import json
 from Stephanie.Modules.base_module import BaseModule
 from Stephanie.language import lang
-from pprint import pprint
+
 
 class WikipediaModule(BaseModule):
     def __init__(self, *args):
         super(WikipediaModule, self).__init__(*args)
 
     def give_a_summary(self):
-        # Translate this \/
         self.assistant.say(_("encyclopedia.search.ask"))
         text = self.assistant.listen().decipher()
-        # add beep sound
         text = text.strip().replace(" ", "%20")
         request = Request(
             'https://' + lang.get_code() + '.wikipedia.org/w/api.php?'
@@ -27,16 +25,15 @@ class WikipediaModule(BaseModule):
                 )
             )
             output = data["query"]["pages"]
+
             # check that the content of output contains a page or not
-            if list(output.keys())[0] is not '-1': # some page has been found
+            if list(output.keys())[0] is not '-1':
                 final = output[list(output.keys())[0]]["extract"]
                 return final
             else:
-                # Translate this \/
                 self.assistant.say(_("encyclopedia.search.no_results"))
                 return None
-            
-            # handle final being None if the page wasn't found
+
             return final
 
         except URLError:
